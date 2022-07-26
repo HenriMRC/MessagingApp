8fabfe19378811943533edfe44551c9f18c321ab
Henrique Marcos <henrimrc@gmail.com>
2022-07-26 20:56:33 +0100

    Client

--- MessagingApp/Client.cs
+++ MessagingApp/Client.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +12,93 @@ namespace MessagingApp
 {
     internal class Client : NetworkBase
     {
+        ClientHandler _handler;
+
         internal override void Start(Action<int, string> onReceiveMessage)
         {
             base.Start(onReceiveMessage);
+
+            string ipBase = GetIPAddress();
+
+            ipBase = ipBase.Substring(0, ipBase.LastIndexOf('.') + 1);
+
+            for (int i = 0; i < 256; i++)
+            {
+                string ip = ipBase + i.ToString();
+
+                Ping ping = new Ping();
+                ping.PingCompleted += new PingCompletedEventHandler(PingCompleted);
+                ping.SendAsync(ip, 1000, ip);
+            }
         }
 
-        internal override void SendMessage(string message)
+        public static string GetIPAddress()
         {
-            throw new NotImplementedException();
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip.ToString();
+
+            return null;
+        }
+
+        private void PingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            string ip = e.UserState as string;
+            if (e.Reply != null)
+            {
+                switch (e.Reply.Status)
+                {
+                    case IPStatus.Success:
+                        string name = null;
+                        try
+                        {
+                            IPHostEntry hostEntry = Dns.GetHostEntry(ip);
+                            name = hostEntry.HostName;
+                        }
+                        catch (SocketException ex)
+                        {
+                            name = "?";
+                            throw;
+                        }
+                        finally
+                        {
+                            Console.WriteLine($"{ip} ({name}) is up: ({e.Reply.RoundtripTime} ms). ");
+                        }
+
+                        try
+                        {
+                            TcpClient client = new TcpClient(ip, 5050);
+
+                            _handler = new ClientHandler(0, client.GetStream(), OnMessageRead, OnConnectionLost);
+                        }
+                        catch (SocketException exception)
+                        {
+                            //Console.WriteLine($"{ip} - SocketException");
+                        }
+                        break;
+                    default:
+                        //Console.WriteLine($"Pinging {ip} completed with state: {e.Reply.Status}");
+                        break;
+                }
+            }
+            else
+            {
+                //Console.WriteLine($"Pinging {ip} failed. (Null Reply object?)");
+            }
+        }
+
+        internal override void SendMessage(string message) => _handler.Write(0, message);
+
+        private void OnMessageRead(int senderID, string message)
+        {
+            OnMessageReceived(senderID, message);
+        }
+
+        private void OnConnectionLost(int clientID, Exception exception)
+        {
+            Debug.Print($"Connection lost with ID {clientID}");
         }
     }
 }
--- MessagingApp/Server.cs
+++ MessagingApp/Server.cs
@@ -120,7 +120,6 @@ namespace MessagingApp
             }
         }
 
-
         private void OnConnectionLost(int clientID, Exception exception)
         {
             lock (_clientsListLock)
@@ -137,127 +136,6 @@ namespace MessagingApp
             }
 
             Debug.Print($"Connection lost with ID {clientID}");
-        }
-
-        private class ClientHandler
-        {
-            internal int ID => _clientID;
-            private readonly int _clientID;
-
-            private readonly NetworkStream _stream;
-
-            private readonly Action<int, string> _onReadCallback;
-            private readonly Action<int, Exception> _onConnectionLost;
-
-            private readonly Queue<byte[]> _writeQueue;
-            private readonly object _writeQueueLock;
-            private bool _writing;
-
-            internal ClientHandler(int clientID, NetworkStream stream, Action<int, string> onReadCallback, Action<int, Exception> onConnectionLost)
-            {
-                _clientID = clientID;
-                _stream = stream;
-
-                _onReadCallback = onReadCallback;
-                _onConnectionLost = onConnectionLost;
-
-                _writeQueue = new Queue<byte[]>();
-                _writeQueueLock = new object();
-
-                _writing = false;
-            }
-
-            internal void Read()
-            {
-                byte[] buffer = new byte[sizeof(int)];
-                _stream.BeginRead(buffer, 0, buffer.Length, MessageSizeCallback, buffer);
-            }
-
-            private void MessageSizeCallback(IAsyncResult result)
-            {
-                try
-                {
-                    _stream.EndRead(result);
-                    byte[] lenghtBuffer = result.AsyncState as byte[];
-                    int length = BitConverter.ToInt32(lenghtBuffer, 0);
-
-                    byte[] buffer = new byte[length];
-                    _stream.BeginRead(buffer, 0, buffer.Length, ReadMessageCallback, buffer);
-                }
-                catch (IOException exception)
-                {
-                    _onConnectionLost.Invoke(_clientID, exception);
-                }
-            }
-
-            private void ReadMessageCallback(IAsyncResult result)
-            {
-                try
-                {
-                    int length = _stream.EndRead(result);
-
-                    byte[] buffer = result.AsyncState as byte[];
-                    string message = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
-
-                    _onReadCallback.Invoke(_clientID, message);
-
-                    Read();
-                }
-                catch (IOException exception)
-                {
-                    _onConnectionLost.Invoke(_clientID, exception);
-                }
-            }
-
-            internal void Write(int senderID, string messageSize)
-            {
-                byte[] messageBytes = Encoding.ASCII.GetBytes(messageSize);
-                List<byte> buffer = new List<byte>(2 * sizeof(int) + messageBytes.Length);
-
-                buffer.AddRange(BitConverter.GetBytes(messageBytes.Length));
-                buffer.AddRange(BitConverter.GetBytes(senderID));
-                buffer.AddRange(messageBytes);
-
-                lock (_writeQueueLock)
-                {
-                    if (_writing)
-                    {
-                        _writeQueue.Enqueue(buffer.ToArray());
-
-                    }
-                    else
-                    {
-                        _stream.BeginWrite(buffer.ToArray(), 0, buffer.Count, WriteMessageCallbac, buffer);
-                        _writing = true;
-                    }
-                }
-            }
-
-            private void WriteMessageCallbac(IAsyncResult result)
-            {
-                try
-                {
-                    _stream.EndWrite(result);
-                }
-                catch (IOException exception)
-                {
-                    _onConnectionLost.Invoke(_clientID, exception);
-                }
-                finally
-                {
-                    lock (_writeQueueLock)
-                    {
-
-                        if (_writeQueue.Count > 0)
-                        {
-                            byte[] buffer = _writeQueue.Dequeue();
-                            _stream.BeginWrite(buffer, 0, buffer.Length, WriteMessageCallbac, buffer);
-                        }
-                        else
-                            _writing = false;
-                    }
-                }
-            }
         }
     }
 }
